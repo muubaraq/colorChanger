a488dcec8cb93f9e4f8be8b77e70b4c6bfacaa24
mubaraq <sixthlogic@gmail.com>
2023-08-21 21:21:28 +0100

    ch: colorcodes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react'        import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+import './App.css'                          import colors from './colors'    function App() {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const currentColor = colors[currentColorIndex];
+  const changeColor = () => {
+    setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);      };
+    return (                                            <>
+      <header className="header">
+   <nav className="nav-div">
+     <div className="logo">                              <h1>Color Flipper</h1>
+     </div>
+     <div className="nav-link">
+       <a href="#">Simple</a>                            <a href="#">Hex</a>                              </div>                                          </nav>
+ </header>
+ <main  style={{backgroundColor:currentColor.hex}}>
+   <div className="container">
+     <div className="color-wrapper">
+      <h2 className="colorName">
+        Background-color: {currentColor.name}
+      </h2>
+     <p>Hex Code: {currentColor.hex} </p>
+     <p>Meaning: {currentColor.meaning}</p>
+      <button className="btn" onClick={changeColor}  style={{backgroundColor: currentColor.hex }}>Change Color</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   </div>
+ </main>
     </>
   )
 }
-
-export default App
+    export default App
---
+++ src/colors.jsx
@@ -0,0 +1,18 @@
+const colors = [  
+{ name: 'Red', hex: '#FF0000', meaning: 'Passion' },
+  { name: 'White', hex: '#FFFFFF', meaning: 'purity'},
+  { name: 'Blue', hex: '#0000FF', meaning: 'Calmness' },
+  { name: 'Green', hex: '#008000', meaning: 'Growth' }, 
+  { name: 'Yellow', hex: '#FFFF00', meaning: 'Happiness' },   
+  { name: 'Purple', hex: '#800080', meaning: 'Creativity' },
+  { name: 'Pink', hex: '#FFC0CB', meaning: 'Love' },
+  { name: 'Orange', hex: '#FFA500', meaning: 'Energy' },
+  { name: 'Brown', hex: '#A52A2A', meaning: 'Stability' },                          
+  { name: 'Gray', hex: '#808080', meaning: 'Balance' },
+  { name: 'Cyan', hex: '#00FFFF', meaning: 'Tranquility' },
+  { name: 'Magenta', hex: '#FF00FF', meaning: 'Spirituality' },
+  { name: 'Teal', hex: '#008080', meaning: 'Clarity' },
+  { name: 'Lavender', hex: '#E6E6FA', meaning: 'Grace' },
+  { name: 'Gold', hex: '#FFD700', meaning: 'Prosperity' },
+  { name: 'Silver', hex: '#C0C0C0', meaning: 'Elegance' },                                          ];                      
+  export default colors;
