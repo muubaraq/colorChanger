a10b93b8308980a8d24dd76953c3f0c2874ccab7
mubaraq <sixthlogic@gmail.com>
2023-08-25 19:59:39 +0100

    ch: toggle color info display

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import colors from './colors'
 function App() {
   const [currentColorIndex, setCurrentColorIndex] = useState(0);
   const currentColor = colors[currentColorIndex];
+  const [displayMode, setDisplayMode] = useState('name'); // 'name' or 'hex'
+
+  const toggleDisplayMode = (mode) => {
+    setDisplayMode(mode);
+  };
   const changeColor = () => {
     setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);      };
     return (                                            <>
@@ -15,15 +20,16 @@ function App() {
      <div className="logo">                              <h1>Color Flipper</h1>
      </div>
      <div className="nav-link">
-       <a href="#">Simple</a>                            <a href="#">Hex</a>                              </div>                                          </nav>
+       <a href="#" onClick={() =>toggleDisplayMode('name')}>Simple</a>                                      <a href="#" onClick={() =>toggleDisplayMode
+('hex')}>Hex</a>
+	    </div>                                          </nav>
  </header>
  <main  style={{backgroundColor:currentColor.hex}}>
    <div className="container">
      <div className="color-wrapper">
       <h2 className="colorName">
-        Background-color: {currentColor.name}
-      </h2>
-     <p>Hex Code: {currentColor.hex} </p>
+          Background-color: {displayMode === 'name' ? currentColor.name : currentColor.hex}
+            </h2>
      <p>Meaning: {currentColor.meaning}</p>
       <button className="btn" onClick={changeColor}  style={{backgroundColor: currentColor.hex }}>Change Color</button>
       </div>
